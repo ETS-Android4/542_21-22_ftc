73ab6fceafee0062afa1c1e1f4a0f256d55f0bac
Nathan Che <59159552+nathanchess@users.noreply.github.com>
2022-02-19 18:37:55 -0800

    CameraAuto Update

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/visionImpl/CameraAutoSid.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/visionImpl/CameraAutoSid.java
@@ -1,22 +1,27 @@
 package org.whitneyrobotics.ftc.teamcode.visionImpl;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.whitneyrobotics.ftc.teamcode.framework.DashboardOpMode;
 import org.whitneyrobotics.ftc.teamcode.visionImpl.ScannerCamSid;
-
-public class CameraAutoSid extends OpMode {
+@TeleOp(name="Vision Test", group="New Tests")
+public class CameraAutoSid extends DashboardOpMode {
     OpenCvWebcam webcam;
     ScannerCamSid scanner = new ScannerCamSid();
+    double scanLevel = 2;
 
     @Override
     public void init() {
+        initializeDashboard(50);
         //int cameraMoniterViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMoniterViewId");
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"));
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "pycam"));
 
         webcam.setPipeline(scanner);
 
@@ -33,13 +38,19 @@ public class CameraAutoSid extends OpMode {
 
             }
         });
-        ScannerCamSid.Barcode result = scanner.getResult();
-        webcam.stopStreaming();
+    }
 
+    @Override
+    public void init_loop(){
+        ScannerCamSid.Barcode result = scanner.getResult();
+        if(result != null){
+            scanLevel = result.ordinal();
+        }
     }
 
     @Override
     public void loop() {
-
+        telemetry.addData("scanned",scanLevel);
+        FtcDashboard.getInstance().startCameraStream(webcam, webcam.getCurrentPipelineMaxFps());
     }
 }
